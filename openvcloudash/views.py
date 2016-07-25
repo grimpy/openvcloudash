f527e332e3efb84d5c74b4d7e896e9d852e43ac7
Jo De Boeck <deboeck.jo@gmail.com>
2016-07-25 21:42:14 +0200

    Make colors nicer

--- openvcloudash/views.py
+++ openvcloudash/views.py
@@ -1,6 +1,12 @@
 from openvcloudash import app, statusses
+from collections import defaultdict
 from flask import render_template
+
+COLORMAP = defaultdict(lambda: 'deep-orange', {'WARNING': 'deep-orange',
+                                          'ERROR': 'red',
+                                          'OK': 'green'}
+                       )
 
 @app.route('/')
 def index():
-    return render_template('index.html', statusses=statusses)
+    return render_template('index.html', statusses=statusses, colormap=COLORMAP)
